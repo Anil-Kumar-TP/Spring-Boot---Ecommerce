f6aff0678c58b90502073000816fb59ab2694cfd
Anil <anil6996f@gmail.com>
2024-12-02 20:42:15 +0530

    added getCart - cart of logged in user

--- src/main/java/com/ecommerce/project/service/CartServiceImplementation.java
+++ src/main/java/com/ecommerce/project/service/CartServiceImplementation.java
@@ -104,6 +104,20 @@ public class CartServiceImplementation implements CartService{
         return cartDTOS;
     }
 
+    @Override
+    public CartDTO getCart(String emailId, Long cartId) {
+        Cart cart = cartRepository.findCartByEmailAndCartId(emailId,cartId);
+        if(cart == null){
+            throw new ResourceNotFoundException("Cart","cartId",cartId);
+        }
+
+        CartDTO cartDTO = modelMapper.map(cart,CartDTO.class);
+        cart.getCartItems().forEach(c->c.getProduct().setQuantity(c.getQuantity()));//or else we will get quantity as in totalStock which is not the quantity that we added.
+        List<ProductDTO> products = cart.getCartItems().stream().map(p -> modelMapper.map(p.getProduct(), ProductDTO.class)).toList();
+        cartDTO.setProducts(products);
+        return cartDTO;
+    }
+
 
     private Cart createCart() {
         Cart userCart = cartRepository.findCartByEmail(authUtil.loggedInEmail());
